6a1ede62e40ebf6907f30d76f435b2ac1129c269
Priska Herger <priska@23bit.net>
2013-03-07 13:57:48 +0100

    renamed matrix to match main.

--- 03_bitplanes/plot_bitplane.m
+++ 03_bitplanes/plot_bitplane.m
@@ -1,10 +1,11 @@
-function plot_bitplane(A, A_bpe)
+function plot_bitplane(A, A_bpd)
 
     figure,
     A = uint8(A);
     subplot(1,2,1), imshow(A);
 
-    A_bpe = uint8(A_bpe);
-    subplot(1,2,2), imshow(A_bpe);
+    A_bpd = uint8(A_bpd);
+    subplot(1,2,2), imshow(A_bpd);
+%     subplot(1,2,2), imshow(fliplr(flipdim(A_bpd,2)))    % for now!
 
 end
