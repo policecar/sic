c451eec7d6f1fe498fc50ef55d2db8c6a356aee8
Priska Herger <priska@23bit.net>
2013-03-07 20:56:11 +0100

    plot_bitplane() now plots the pSNRs as well.

--- 03_bitplanes/plot_bitplane.m
+++ 03_bitplanes/plot_bitplane.m
@@ -1,4 +1,4 @@
-function plot_bitplane(A, A_bpd)
+function plot_bitplane(A, A_bpd, peak)
 
     figure,
     A = uint8(A);
@@ -6,5 +6,12 @@ function plot_bitplane(A, A_bpd)
 
     A_bpd = uint8(A_bpd);
     subplot(1,2,2), imshow(A_bpd);
+    
+    figure, plot(peak,'x-g');
+    title('Peak Signal-to-Noise Ratio')
+    xlabel('Number of planes'), ylabel('Peak SNR')
+    set(gca,'YScale','log')
+    set(gca,'XTick', [2,3,4,5,6,7])
+    set(gca,'XTickLabel',{'1','2','3','4','5','6'})
 
 end
