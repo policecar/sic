0354839d6a9b02aeb541232aff64480ee1e61813
Priska Herger <priska@23bit.net>
2013-03-08 16:36:57 +0100

    refactored, made prettier.

--- main.m
+++ main.m
@@ -1,10 +1,10 @@
 function main(fname)
-%MAIN       Calls a day's code with the filename provided in fname if any.
+%MAIN       Calls all code with a provided filename if any.
 
     % set default filename
     if (nargin < 1)
-        %filename = 'baboon.tiff';
-        filename = 'clock.tiff';
+        filename = 'baboon.tiff';
+        %filename = 'clock.tiff';
     else
         filename = fname;
     end
@@ -12,66 +12,65 @@ function main(fname)
     addpath('01_farbraeume');
     addpath('03_bitplanes');
 
+    DATA_DIR = 'images/color/';
+    %DATA_DIR = 'images/gray/';
     OUTPUT_DIR = 'output/';
+    
     fmt = 'tiff';
+    [~, fn, ~] = fileparts(filename);
 
     %profile on  % profile your code
     
-%     DATA_DIR = 'images/color/';
-% 
-%     % open image
-%     A = imread(strcat(DATA_DIR, filename), fmt);
-% 
-%     % process image
-%     A_rgb = double(A);                  % convert uint8 to double
-%     A_yuv = rgb2yuv(A_rgb);             % transform RGB to YUV
-%     [A_420, up] = subsampler420(A_yuv); % chroma subsampling 4:2:0
-% 
-%     % save image
-%     imwrite(A_yuv, strcat(OUTPUT_DIR, 'yuv_', filename), fmt);
-
-
-    %% day 3
-    DATA_DIR = 'images/gray/';
-    
-    % load image
+    % open image
     A = imread(strcat(DATA_DIR, filename), fmt);
-    
-    % if image of color, convert to YUV first, process Y channel only
-    if size(A,3) > 1
-        A = rgb2yuv(A);
-    end
-    A = A(:,:,1);
+ 
     
     % process image
-    A = double(A);  % convert uint8 to double
-    %A = [183 -4; -10.2, 11.5];
-    cuts = [128,64,32,16,8,4,2,1];
-    %cuts = [64];
-    [m,n] = size(A);
-    peak = zeros(size(cuts));
-    A_bpd = zeros(size(A));
-    for j = 1:numel(cuts)
-        % encode matrix to bit plane encoding
-        display('Bit plane encoding')
-        tic
-        Bs = bitplaneEncoder(A(:,:), cuts(j));
-        toc
-        % decode bit stream from bit plane decoding
-        display('Bit plane decoding')
-        tic
-        threshold = max(max(A(:,:)));
-        A_bpd(:,:) = bitplaneDecoder(Bs, threshold, m, n);
-        toc
-        % compute PSNR
-        peak(j) = psnr(A, A_bpd, threshold);
-    end
+    A_rgb = double(A);      % convert uint8 to double
     
-    % visualize transformed image
-    plot_bitplane(A, A_bpd, peak, cuts, threshold)
+    % RGB to YUV transformation
+    A_yuv = rgb2yuv(A_rgb);
+    
+    % chroma subsampling 4:2:0
+    tic, [A_sub, A_up] = subsampling420(A_yuv); toc
+    
+    % bit plane coding
+    A_y = A_yuv(:,:,1);     % process only the Y channel
+    [m,n] = size(A_y);
+    threshold = max(A_y(:));
+    cut = [128,64,32,16,8,4,1];
+    peak = zeros(size(cut));
+    figure,
+    subplot(2,4,1), imshow(A);
+    title('original image'),
+    for c = 1:numel(cut)
+        tic, Bs = bitplaneEncoding(A_y, cut(c)); toc
+        tic, A_bp = bitplaneDecoding(Bs, threshold, m, n); toc
+        peak(c) = psnr(A_yuv(:,:,1), A_bp, threshold);
+        numPlanes = ceil(log2(threshold) - log2(cut(c)));
+        subplot(2,4,c+1), imshow(uint8(A_bp));
+        title(strcat(num2str(numPlanes), ' planes')),
+    end
+    saveas(gcf, strcat(OUTPUT_DIR, fn, '_bp_comp'), 'png')
+    
+    % wavelet transformation
+    %teuxdeux
+
+    
+    % make some plots and save them to disc
+    plot_yuv(A_yuv);        % plot each YUV channel separately
+    saveas(gcf, strcat(OUTPUT_DIR, fn, '_yuv'), 'png')
+
+    plot_sub(A_yuv, A_sub, A_up)
+    saveas(gcf, strcat(OUTPUT_DIR, fn, '_sub'), 'png')
+    
+    plot_bitplanes(A, A_bp)
+    saveas(gcf, strcat(OUTPUT_DIR, fn, '_bitplanes'), 'png')
+    plot_psnr(peak, threshold, cut)
+    saveas(gcf, strcat(OUTPUT_DIR, fn, '_psnr'), 'png')
     
     % save image
-    imwrite(A_bpd, strcat(OUTPUT_DIR, 'bpd_', filename), fmt)
+    % ...
     
     %stats = profile('info');
     %profile off
