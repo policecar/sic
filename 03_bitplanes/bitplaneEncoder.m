d46639c9637f425236d19de26146688981ac698f
Priska Herger <priska@23bit.net>
2013-03-08 11:03:17 +0100

    cleaned up bitplaneEncoder a bit, added moa comments.

--- 03_bitplanes/bitplaneEncoder.m
+++ 03_bitplanes/bitplaneEncoder.m
@@ -7,13 +7,19 @@ function B = bitplaneEncoder(A, cut)
     %   01    unknown as yet
     %   10    significant and positive 
     %   11    significant and negative
-
+    
+    % temporary significance coding:
+    %   0     -> 00
+    %   1     -> 01
+    %   2     -> 10
+    %   3     -> 11
+    
     % set threshold to max value in A floored to the nearest power of 2
     threshold = max(max(A));
     threshold = 2^(floor(log2(threshold)));
     n = numel(A);
     L_up = zeros(size(A));  % a look-up table to store significant entries
-    Aa = A; % matrix for refinement decisions
+    Aa = A;                 % a matrix to store shaved entry values
     
     % instantiate bit stream (to max required, cut off later on)
     steps = ceil(log2(threshold) - log2(cut));  % correct !?
@@ -22,51 +28,43 @@ function B = bitplaneEncoder(A, cut)
     
     thr = threshold;
     
-    % loop over threshold until reaching cut
-    % in each loop and per pixel write either two significance bits or a
-    % single refinement bit to the bit stream
+    % loop over threshold until cut is reached
+    % in each loop and per pixel write either two significance bits or one
+    % refinement bit to the bit stream
     while thr >= cut
       
-        A_sig = zeros(size(A));
-        A_ref = zeros(size(A));
+        A_sig = zeros(size(A)); % instantiate matrix for significance val.
+        A_ref = zeros(size(A)); % instantiate matrix for refinement values
         i = 1; 
         while i <= n
 
             % significance pass
             if (abs(A(i)) > thr) && ...
                (thr == threshold || abs(A(i)) <= thr*2)
-                L_up(i) = 1;
-                % mark if value has been significant before
-                % which is the case if it's value > threshold
+                L_up(i) = 1; % mark significant entries
+                % encode encountered value, cf. temporary coding above
                 if sign(A(i)) == -1
                     A_sig(i) = 3;
                 else
                     A_sig(i) = 2;
                 end
-                Aa(i) = Aa(i) - thr;
+                Aa(i) = Aa(i) - thr; % subtract current threshold
             end
 
             % refinement pass
-            % if pixel has been significant but not in this round,
-            % evaluate refinement
+            % if pixel has been significant before but not in this round,
+            % evaluate and send refinement, else send significance value
             if L_up(i) == 1 && A_sig(i) == 0
                 % make it pretty: dissect number into powers of 2, check
                 % if log2(thr) is in that list
-                %if A(i) >= thr     % incorrect! but interesting enough
                 if Aa(i) >= thr
                     A_ref(i) = 1;
                     Aa(i) = Aa(i) - thr;
                 end
-            end
-
-            % decide on bit stream value to transfer
-            % if entry has been significant but not in current round, 
-            % use refinement value, else use significance value
-            if L_up(i) == 1 && A_sig(i) == 0
                 B(ix,:) = A_ref(i);
                 ix = ix+1;
             else
-                % reverse mapping: 0 -> 00, 1 -> 01, 2 -> 10, 3 -> 11
+                % reverse temporary mapping, cf. temporary coding above
                 tmp = A_sig(i);
                 if tmp < 2
                     B(ix,1) = 0;
@@ -83,12 +81,11 @@ function B = bitplaneEncoder(A, cut)
                         B(ix+1,1) = 1;
                     end
                 end
-                ix = ix+2;
+                ix = ix+2;                
             end
-                
             i = i+1;
         end
-        thr = thr/2;    % adapt threshold
+        thr = thr/2; % adapt threshold
     end
     B = B(1:ix-1,:);
        
