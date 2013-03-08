9db2cf6b6528b4de8bf3c6394fce2887e3c0bb0f
Priska Herger <priska@23bit.net>
2013-03-08 11:50:20 +0100

    corrected threshold for 3-D matrices.

--- main.m
+++ main.m
@@ -15,6 +15,8 @@ function main(fname)
     OUTPUT_DIR = 'output/';
     fmt = 'tiff';
 
+    %profile on  % profile your code
+    
 %     DATA_DIR = 'images/color/';
 % 
 %     % open image
@@ -31,29 +33,31 @@ function main(fname)
 
     %% day 3
     DATA_DIR = 'images/gray/';
+    %DATA_DIR = 'images/color/';
     
     % load image
     A = imread(strcat(DATA_DIR, filename), fmt);
     
     % process image
     A = double(A);  % convert uint8 to double
-    %A = [3 -4; -10, 11];
-    [m,n] = size(A);
+    %A = [183 -4; -10.2, 11.5];
     cuts = [128,64,32,16,8,4,2,1];
+    %cuts = [64];
+    sz = size(A);
     peak = zeros(size(cuts));
-    threshold = max(max(A));
-    A_bpd = zeros(size(A));
+    A_bpd = zeros(sz);
     for j = 1:numel(cuts)
-        for i = 1:size(A,3)
+        for i = 1:size(A,3) % (don't use sz(3), will throw error)
             % encode matrix to bit plane encoding
-            display('do bitplane encoding')
+            display('Bit plane encoding')
             tic
-            A_bpe = bitplaneEncoder(A(:,:,i), cuts(j));
+            Bs = bitplaneEncoder(A(:,:,i), cuts(j));
             toc
             % decode bit stream from bit plane decoding
-            display('do bitplane decoding')
+            display('Bit plane decoding')
             tic
-            A_bpd(:,:,i) = bitplaneDecoder(A_bpe, threshold, m, n);
+            threshold = max(max(A(:,:,i)));
+            A_bpd(:,:,i) = bitplaneDecoder(Bs, threshold, sz(1), sz(2));
             toc
         end
         % compute PSNR
@@ -65,5 +69,8 @@ function main(fname)
     
     % save image
     imwrite(A_bpd, strcat(OUTPUT_DIR, 'bpd_', filename), fmt)
+    
+    %stats = profile('info');
+    %profile off
 
 end
