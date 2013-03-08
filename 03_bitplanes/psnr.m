fc2c3a72cacbf04fc8038b5abe5bd6f5c0db817e
Priska Herger <priska@23bit.net>
2013-03-08 10:34:35 +0100

    corrected another error in the peak SNR calculation.

--- 03_bitplanes/psnr.m
+++ 03_bitplanes/psnr.m
@@ -2,7 +2,7 @@ function p = psnr(A, A_bp, threshold)
 %PSNR       Computes the Peak Signal-to-Noise ratio (PSNR)
 
     n = numel(A);
-    ds = (A_bp - A) ^2;                     % the difference squared
+    ds = (A_bp - A) .^2;                    % the difference squared
     MSE = (1 /n) * sum(ds(:));              % the mean squared error
     maxVal = 2^(ceil(log2(threshold))) -1;  % the maximal possible value
     
