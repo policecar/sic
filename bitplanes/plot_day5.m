8939806fd81e4a3e13dd8b21adc487c478690cf0
Priska Herger <priska@23bit.net>
2013-03-11 22:22:05 +0100

    changed plot to show difference.

--- bitplanes/plot_day5.m
+++ bitplanes/plot_day5.m
@@ -1,19 +1,18 @@
 
-function plot_day5(A, A_yuv, A_bp, A_sub)
+function plot_day5(A, A_yuv, A_bp)
 
     figure,
     
     subplot(1,3,1), imshow(A),
     title('original image')
 
-    Aa = A_yuv; A(:,:,1) = A_bp;
+    Aa = A_bp;
     Aa = yuv2rgb(Aa);
     subplot(1,3,2), imshow(uint8(Aa)),
-    title('bitplane coded Y channel')
+    title('bitplane coded image')
     
-    Aa = A_sub; Aa(:,:,1) = A_bp;
-    Aa = yuv2rgb(Aa);
-    subplot(1,3,3), imshow(uint8(Aa)),
-    title('bitplane coded Y and chroma subsampled UV channels')
+    A_diff = A_yuv - A_bp;
+    subplot(1,3,3), imshow(uint8(yuv2rgb(A_diff))),
+    title('diff b/w original and bitplane coding')
 
 end
