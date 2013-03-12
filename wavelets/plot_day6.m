47b06af1667f3da50c0a418b4576ba2b779e07dd
Priska Herger <priska@23bit.net>
2013-03-12 18:33:06 +0100

    some changes.

--- wavelets/plot_day6.m
+++ wavelets/plot_day6.m
@@ -1,16 +1,13 @@
     
-function plot_day6(A_yuv, A_haar, A_h)
+function plot_day6(A_yuv, T, Y)
 
     figure,
     
-    subplot(1,3,1), imshow(uint8(yuv2rgb(A_yuv))),
-    title('Original image')
-    
-    subplot(1,3,2), imshow(uint8(A_haar)),
+    subplot(1,2,1), imshow(uint8(T)),
     title('Haar encoded image')
     
-    A_h(:,:,2:3) = A_yuv(:,:,2:3);
-    subplot(1,3,3), imshow(uint8(yuv2rgb(A_h))),
-    title('Haar transformed image')
+    A_diff = A_yuv(:,:,1) - Y;
+    subplot(1,2,2), imagesc(A_diff),
+    title('Diff b/w Y and original')
     
 end
