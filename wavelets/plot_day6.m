e0cac4e449351d660324735d12cf5b74c89460ec
Priska Herger <priska@23bit.net>
2013-03-13 08:30:57 +0100

    added a wavelet transform plot; generalized a title.

--- wavelets/plot_day6.m
+++ wavelets/plot_day6.m
@@ -3,11 +3,14 @@ function plot_day6(A_yuv, T, Y)
 
     figure,
     
-    subplot(1,2,1), imshow(uint8(T)),
-    title('Haar encoded image')
+    subplot(1,3,1), imshow(uint8(T)),
+    title('Wavelet encoded image')
     
+    subplot(1,3,2), imshow(uint8(Y)),
+    title('Wavelet transformed image')
+        
     A_diff = A_yuv(:,:,1) - Y;
-    subplot(1,2,2), imagesc(A_diff),
+    subplot(1,3,3), imagesc(A_diff),
     title('Diff b/w Y and original')
     
 end
