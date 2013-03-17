91a1f9ecaeac1ff3f8ba208cf9f60773e1941971
Priska Herger <priska@23bit.net>
2013-03-17 14:13:03 +0100

    plot wavelet transformation standalone.

--- plots/plot_wavelets.m
+++ plots/plot_wavelets.m
@@ -1,22 +1,28 @@
 
-function plot_wavelets(YUV, Y_enc, Y_dwt)
-%PLOT_WAVELETS  Takes a Y channel, its wavelet encoded version (Haar or
-%               Daubechies) and its wavelet transformation and plots them.
+function plot_wavelets(Y, wavelet)
+%PLOT_WAVELETS  Plots the original Y channel, its wavelet encoding and its
+%               wavelet transform (Haar or Daubechies).
 
     figure,
-
-    Y = YUV(:,:,1);
+    
+    Y_enc = DWTEncoder(Y, wavelet);
+    Y_dec = DWTDecoder(Y_enc, wavelet);
+    
+    % watch for data scale!
+    
     subplot(1,4,1), imshow(uint8(Y)),
-    title('original Y channel')
+    title('Original Y channel')
 
     subplot(1,4,2), imshow(uint8(Y_enc)),
-    title('Wavelet encoding')
+    title('Wavelet encoded Y channel')
     
-    subplot(1,4,3), imshow(uint8(Y_dwt)),
-    title('Wavelet transformation')
-        
-    Diff = Y - Y_dwt;
-    subplot(1,4,4), imshow(uint8(Diff)),
-    title('Diff b/w Y and Y\_dwt')
+    subplot(1,4,3), imshow(uint8(Y_dec)),
+    title('Wavelet transformed Y channel')
+    
+    subplot(1,4,4), imshow(uint8((abs(Y - Y_dec)*10))),
+    title('Diff b/w original and wavelet transform')
+
+    %figure,
+    %imagesc(Y - Y_dec),
     
 end
