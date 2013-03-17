9cc8f9b4e2746e30bf07f0b9cbe8cd5b09bbce51
Priska Herger <priska@23bit.net>
2013-03-17 22:20:07 +0100

    refactored main, corrected processing order.

--- main.m
+++ main.m
@@ -11,11 +11,7 @@ function main(fname)
         filename = fname;
     end
 
-    addpath('farbraeume');
-    addpath('bitplanes');
-    addpath('wavelets');
-    addpath('measures');
-    addpath('plots');
+    addpath(genpath('./'))              % add subdirectories to path
 
     DATA_DIR = 'images/color/';
     %DATA_DIR = 'images/gray/';
@@ -28,62 +24,63 @@ function main(fname)
     % open image
     A = imread(strcat(DATA_DIR, filename));
     
+    
     % process image
-    % preprocessing
     display('preprocessing')
     A_rgb = double(A);                  % convert uint8 to double
     A_yuv = rgb2yuv(A_rgb);             % RGB to YUV transformation   
     [Y, U, V] = subsampling420(A_yuv);  % chroma subsampling 4:2:0
-    A_up = upsampling420(Y,U,V);
     
-    % discrete wavelet tranformation
-    display('wavelet transformation')
+    display('wavelet encoding')
     wavelet = 0;                        % choose b/w Haar and Daubechies
     Y_wv = DWTEncoder(Y, wavelet);
-    Y_dwt = DWTDecoder(Y_wv, wavelet);
     U_wv = DWTEncoder(U, wavelet);
-    U_dwt = DWTDecoder(U_wv, wavelet);
     V_wv = DWTEncoder(V, wavelet);
-    V_dwt = DWTDecoder(V_wv, wavelet);
-    
-    % temporary hack until bit plane coding works w/ diff. sizes
-    A_wv = upsampling420(Y_wv,U_wv,V_wv);
-    A_dwt = upsampling420(Y_dwt, U_dwt, V_dwt);
-    
+        
     % bit plane coding (potentially zero-tree pimped)
-    display('bit plane coding')
-    numIter = 7;
+    display('bit plane encoding')
+    numIter = ceil(log2(max(abs(Y_wv(:))))) +3;
     tic,
-    Bs = BitplaneEncoding(A_dwt(:,:,1),A_dwt(:,:,2),A_dwt(:,:,3), numIter);
-    %Bs = BitplaneZerotreeEncoding(A_dwt(:,:,1), A_dwt(:,:,2), ...
-    %                                A_dwt(:,:,3),numIter);
+    Bs = BitplaneEncoding(Y_wv, U_wv, V_wv, numIter);
+    %Bs = BitplaneZerotreeEncoding(Y_wv, U_wv, V_wv, numIter);
     toc
     %pause                              % pause for bitstream manipulation
-    tic, [A_bp, Planes] = BitplaneDecoding(Bs); toc
-    %tic, A_bp = BitplaneZerotreeDecoding(Bs); toc
+    display('bit plane decoding')
+    tic,
+    [Y_bp, U_bp, V_bp] = BitplaneDecoding(Bs);
+    %[Y_bp, U_bp, V_bp] = BitplaneZerotreeDecoding(Bs);
+    toc
     
-    % postprocessing
+    display('wavelet decoding')
+    Y_dwt = DWTDecoder(Y_bp, wavelet);
+    U_dwt = DWTDecoder(U_bp, wavelet);
+    V_dwt = DWTDecoder(V_bp, wavelet);
+
     display('postprocessing')
-    Y = A_bp(:,:,1);
-    A_r = upsampling420(Y, U, V);       % chroma upsample 4:2:0
-    A_res = yuv2rgb(A_r);               % convert YUV to RGB color space
+    A_up = upsampling420(Y_dwt, U_dwt, V_dwt); % chroma upsampling 4:2:0
+    A_res = yuv2rgb(A_up);               % convert YUV to RGB color space
     
     
     % make some plots and save them to disc
+    display('make plots')
     plot_yuv(A_rgb, A_yuv);
     saveas(gcf, strcat(OUTPUT_DIR, fn, '_yuv'), 'png')
     
-    plot_sub(A_yuv, A_up);
+    plot_sub(A_yuv, Y, U, V);
     saveas(gcf, strcat(OUTPUT_DIR, fn, '_sub'), 'png')
-    
-    plot_bitplanes(A_yuv, Planes);
-    saveas(gcf, strcat(OUTPUT_DIR, fn, '_bitplanes'), 'png')
-    
-    plot_wavelets(A_yuv(:,:,1), Y_wv, Y_dwt);
+
+    plot_wavelets(Y, wavelet);
     saveas(gcf, strcat(OUTPUT_DIR, fn, '_wavelets'), 'png')
+
+    plot_wavelets2(Y);
+    saveas(gcf, strcat(OUTPUT_DIR, fn, '_wavelet_comp'), 'png')
     
-    %plot_RD(A_yuv)
-    %saveas(gcf, strcat(OUTPUT_DIR, fn, '_rd'), 'png')
+    %display('show some analysis')
+    %plot_DR(A_yuv)
+    %saveas(gcf, strcat(OUTPUT_DIR, fn, '_dr'), 'png')
+    
+    %plot_mE(A_yuv(:,:,1))
+    %saveas(gcf, strcat(OUTPUT_DIR, fn, '_me'), 'png')
     
     figure,
     subplot(1,2,1), imshow(uint8(A_rgb)), title('The original'),
