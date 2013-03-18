89c90cbf6d9c973373e277827fee662736ada0f9
Priska Herger <priska@23bit.net>
2013-03-19 00:14:40 +0100

    started using conv2's SHAPE parameter. enhanced doc.

--- wavelets/sconv.m
+++ wavelets/sconv.m
@@ -1,30 +1,25 @@
 
 function H = sconv(Filter, Matrix)
-%SCONV      Row-wise symmetric convolution of a filter given as row vector 
-%           with a matrix.
+%SCONV      Row-wise symmetric convolution of a filter with a matrix.
     
     % helper variables
-    n = length(Matrix); % length of input matrix
-    f = length(Filter); % length of filter
-    l = f + n - 1;      % length of symmetrically extended matrix
-    fhalbe = (f-1)/2;	% length of half the filter (ceiled)
-    z = 2 * n - 2;		% length of circular index
+    n = length(Matrix);		% length of input matrix
+    f = length(Filter);		% length of filter
+    l = f + n - 1;			% length of symmetrically extended matrix
+    hf = (f - 1) / 2;	% length of half the filter (ceiled)
+    z = 2 * n - 2;			% length of circular index, e.g. cbabcd
     
     % symmetric extension
-    E = zeros(n,l);
+    E = zeros(n,l);			% extended matrix
 	for col = 1:length(E),
 
-		i =  mod((col - 1) - fhalbe, z) + 1; % get z index of pixel value 
-											 % to copy
-        j = n - abs(i - n);		% map index to actual index in Matrix
-        E(:,col) = Matrix(:,j);	% copy respective column to extended matrix
+		i =  mod((col - 1) - hf, z) + 1; % get z index of pixel to copy
+        j = n - abs(i - n);		% map z index to index in Matrix
+        E(:,col) = Matrix(:,j);	% copy respective M column to E matrix
 
 	end
 	
     % convolute
-    H = conv2(Filter, E);
-    
-    % trim matrix
-    H = H(:,2*fhalbe+1:2*fhalbe+n);
+	H = conv2(E, Filter, 'valid');
     
 end
